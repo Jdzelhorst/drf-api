3e344c60b4c6f270409599587d33aa1a71350da1
Jan Douwe Zelhorst <110466722+Jdzelhorst@users.noreply.github.com>
2023-11-14 14:41:20 +0000

    tweaked settings for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -67,7 +67,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-AALLOWED_HOSTS = [
+ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     'interactopia-api-e9e9ddc66214.herokuapp.com',
     'localhost',
